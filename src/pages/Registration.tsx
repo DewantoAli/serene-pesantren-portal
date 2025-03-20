68da793c92b1ed94acbdc56020bee7949ddd6c07
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2025-03-20 14:16:39 +0800

    Update Registration.tsx

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -10,7 +10,7 @@ import GoogleScriptSetupGuide from '@/components/GoogleScriptSetupGuide';
 const Registration: React.FC = () => {
   const [step, setStep] = useState(1);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [googleScriptUrl, setGoogleScriptUrl] = useState('');
+  const [googleScriptUrl, setGoogleScriptUrl] = useState('https://script.google.com/macros/s/AKfycbxEzSJ7GkECBJ_hFe4EGhcmqe9pmCTfgPCvvYB0cEC8oVr8BHjnnz0hKJXCUAxYL5MFbQ/exec');
   const [formData, setFormData] = useState({
     // Personal Information
     firstName: '',
