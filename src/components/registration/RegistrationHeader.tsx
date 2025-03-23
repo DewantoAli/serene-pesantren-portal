c67a21f6e56904783d727a710828fbd926674165
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2025-03-24 05:42:36 +0800

    Update RegistrationHeader.tsx

--- src/components/registration/RegistrationHeader.tsx
+++ src/components/registration/RegistrationHeader.tsx
@@ -34,9 +34,8 @@ const RegistrationHeader: React.FC = () => {
                        <div className="w-5 h-5 rounded-full bg-islamic-teal/20 flex items-center justify-center mt-1 mr-3">
                         <div className="w-2 h-2 rounded-full bg-islamic-teal"></div>
                       </div>
-                      <span>{item}</span>
-                    </li>
-                  ))}
+                   
+                  ]}
                 </ul>
               
         
