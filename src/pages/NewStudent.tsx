14b0481ff602955b88f8955bcf34b55532fa4bf4
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2025-04-22 09:19:19 +0800

    Update NewStudent.tsx

--- src/pages/NewStudent.tsx
+++ src/pages/NewStudent.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import AnimatedSectionWrapper from '@/components/ui/AnimatedSectionWrapper';
