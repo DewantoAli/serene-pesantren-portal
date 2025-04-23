df0d9673761808e0431a8cbdc9c3c71e875fc440
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2025-04-23 09:55:29 +0800

    Update NewStudent.tsx

--- src/pages/NewStudent.tsx
+++ src/pages/NewStudent.tsx
@@ -12,7 +12,7 @@ const NewStudent: React.FC = () => {
     <>
       <Header />
       <main className="">
-        <div className="container mx-auto px-4 md:px-6 relative z-10">
+        <div className="">
           <AnimatedSectionWrapper className="text-center mb-8">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-serif font-bold text-islamic-navy mb-4">
               Pendaftaran Santri Baru
