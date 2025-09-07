7add5c015ac95d78d63df7beca1be61c5a16e56e
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2025-09-07 15:07:09 +0800

    Update VideoReelSection.tsx

--- src/components/ui/VideoReelSection.tsx
+++ src/components/ui/VideoReelSection.tsx
@@ -13,7 +13,7 @@ const VideoReelSection: React.FC = () => {
     },
     {
       id: 2,
-      thumbnail: "/lovable-uploads/df6366a9-1018-4237-bffb-263beadecc59.png",
+      thumbnail: "https://www.youtube.com/shorts/ooKSulHGEu8",
       title: "",
       url: 'https://www.youtube.com/shorts/ooKSulHGEu8',
       platform: "https://www.youtube.com/shorts/ooKSulHGEu8",
