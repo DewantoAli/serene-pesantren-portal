9882a2f837f3b209566346cad83fbbeeef2e6b8c
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2025-09-14 10:04:18 +0800

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -159,8 +159,7 @@ const Footer: React.FC = () => {
                 <Mail size={18} className="text-islamic-gold mr-2" />
                 <span className="text-sm text-islamic-sand/80">irsyadulhaq.manado@gmail.com</span>
               </li>
-              
-            
+            </ul>           
           </div>
         </div>
         
