cf43fadfe6cb5875a049a441e313ddba4f4d5e49
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2026-01-05 07:41:19 +0800

    Update StatusPembayaranKelas7.tsx

--- src/pages/StatusPembayaranKelas7.tsx
+++ src/pages/StatusPembayaranKelas7.tsx
@@ -11,7 +11,7 @@ const StatusPembayaranKelas7: React.FC = () => {
           <h1 className="text-2xl font-bold text-islamic-navy mb-4">Status Pembayaran SPP - Kelas VII</h1>
         </div>
         <iframe
-          src="https://docs.google.com/spreadsheets/d/1YqHZHXsanr1-8R9OQYcr92xsrsLcb_87lMs93TWWe0I/edit?gid=1198977414#gid=1198977414?widget=true&headers=false"
+          src="https://docs.google.com/spreadsheets/d/1YqHZHXsanr1-8R9OQYcr92xsrsLcb_87lMs93TWWe0I/pubhtml?widget=true&headers=false"
           className="w-full flex-1 border-0"
           style={{ height: 'calc(100vh - 200px)' }}
           title="Status Pembayaran SPP Kelas VII"
