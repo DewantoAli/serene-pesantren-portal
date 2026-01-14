e28e9a4c080acdc28b49112078a28f759f733ce7
DewantoAli <42129467+DewantoAli@users.noreply.github.com>
2026-01-15 05:44:28 +0800

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -65,7 +65,7 @@ const Header: React.FC = () => {
   ];
 
   const aplikasiSubMenu = [
-    { name: 'Keuangan Pesantren', path: '/aplikasi-pesantren' },
+    { name: 'Administrasi & Keuangan Pesantren', path: '/aplikasi-pesantren' },
   ];
 
   const statusPembayaranPath = '/status-pembayaran-spp';
